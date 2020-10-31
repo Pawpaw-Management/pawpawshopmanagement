79386f116e856f78ac77b2c5040e6bbaef3ddada
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-10-30 22:51:03 -0600

    edited router and removed unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
-import CreateAccount from "./Components/Customers/CreateAccount/CreateAccount.js";
-import SearchAccount from "./Components/Customers/SearchAccount/SearchAccount.js";
-import Employees from "./Components/Employees/Employees.js";
+import Appointments from "./Components/Appointments/Appointments";
+import Customers from "./Components/Customers/Customers";
+import Employees from "./Components/Employees/Employees";
 
 function App(props) {
    return (
       <Router>
-         <div className="App">
+         <div className="app">
             <Switch>
-               <Route path="/createAccount">
-                  <CreateAccount />
+               <Route path="/customers">
+                  <Customers />
                </Route>
-               <Route path="/searchAccount">
-                  <SearchAccount allInfo={props.allInfo} />
+               <Route path="/appointments">
+                  <Appointments allInfo={props.allInfo} />
                </Route>
                <Route path="/employees">
                   <Employees />
                </Route>
             </Switch>
-            <nav className="NavButtons">
-               <Link to="/createAccount">
-                  <button>Appointment</button>
+            <nav className="nav">
+               <Link to="/appointments">
+                  <button>Appointments</button>
                </Link>
-               <Link to="/searchAccount">
-                  <button>Customer</button>
+               <Link to="/customers">
+                  <button>Customers</button>
                </Link>
                <Link to="/employees">
                   <button>Employees</button>
