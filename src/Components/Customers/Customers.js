72e45a1d4695adfe229804059f583779805116af
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-10-30 22:47:23 -0600

    created customers component structure

--- src/Components/Customers/Customers.js
+++ src/Components/Customers/Customers.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import CreateAccount from "./CreateAccount/CreateAccount"
+import SearchAccount from "./SearchAccount/SearchAccount"
+import "../CommonElements.css"
 
 export default class Customers extends Component {
-    render() {
-        return (
-            <div>
-                
+   render() {
+      return (
+         <Router>
+            <div className="customers">
+               <Switch>
+                  <Route path="/create">
+                     <CreateAccount />
+                  </Route>
+                  <Route path="/search">
+                     <SearchAccount />
+                  </Route>
+               </Switch>
+               <nav className="nav-components">
+                  <Link to="/create">
+                     <button>Create New Customer Account</button>
+                  </Link>
+                  <Link to="/search">
+                     <button>Search/Edit Customer Account</button>
+                  </Link>
+               </nav>
             </div>
-        )
-    }
+         </Router>
+      );
+   }
 }
