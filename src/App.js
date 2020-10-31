a7e396a9ce43f31d9c376a32fafd597321449836
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-10-30 21:33:59 -0600

    simplified app_js by eliminating unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
-import CreateAccount from "./Components/CreateAccount/CreateAccount.js";
-import SearchAccount from "./Components/SearchAccount/SearchAccount.js";
-import CreateAccountButton from "./Components/NavBar/CreateAccountButton/CreateAccountButton.js";
-import SearchAccountButton from "./Components/NavBar/SearchAccountButton/SearchAccountButton.js";
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import CreateAccount from "./Components/Customers/CreateAccount/CreateAccount.js";
+import SearchAccount from "./Components/Customers/SearchAccount/SearchAccount.js";
+import Employees from "./Components/Employees/Employees.js";
 
 function App(props) {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <Switch>
-            <Route path="/createAccount">
-              <CreateAccount />
-            </Route>
-            <Route path="/searchAccount">
-              <SearchAccount allInfo={props.allInfo} />
-            </Route>
-          </Switch>
-          <nav className="NavButtons">
-            <Link to="/createAccount">
-              <CreateAccountButton />
-            </Link>
-
-            <Link to="/searchAccount">
-              <SearchAccountButton allInfo={props.allInfo} />
-            </Link>
-          </nav>
-        </header>
-      </div>
-    </Router>
-  );
+   return (
+      <Router>
+         <div className="App">
+            <Switch>
+               <Route path="/createAccount">
+                  <CreateAccount />
+               </Route>
+               <Route path="/searchAccount">
+                  <SearchAccount allInfo={props.allInfo} />
+               </Route>
+               <Route path="/employees">
+                  <Employees />
+               </Route>
+            </Switch>
+            <nav className="NavButtons">
+               <Link to="/createAccount">
+                  <button>Appointment</button>
+               </Link>
+               <Link to="/searchAccount">
+                  <button>Customer</button>
+               </Link>
+               <Link to="/employees">
+                  <button>Employees</button>
+               </Link>
+            </nav>
+         </div>
+      </Router>
+   );
 }
 
 export default App;
