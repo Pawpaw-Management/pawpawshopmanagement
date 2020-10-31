c7f33d081175cedcbdf5ab576dce26ca39883500
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-10-30 21:41:44 -0600

    created folder structure for all major components

---
+++ src/Components/Employees/Employees.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Employees extends Component {
+    render() {
+        return (
+            <div>
+                <button>Add New Employee</button>
+                <button>Edit Employee</button>
+            </div>
+        )
+    }
+}
