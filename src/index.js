3363e7943b64b9c772a8f706ea40e3988d27ac0e
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-11-04 14:39:30 -0700

    fetch all data from db

--- src/index.js
+++ src/index.js
@@ -1,28 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-// Fetch data from server
-// const getCustomersInfo = async function () {
-//   const response = await fetch("http://localhost:1337/customers", {
-//     "method": "GET",
-//     "headers": {
-//       "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTk3Nzc0NTgxLCJleHAiOjE2MDAzNjY1ODF9.unQELe_ldBhcVIzj3Jf9TQd7l3X6v62Jtr0zamJ3P14"
-//     }
-//   });
-//   var customersInfo = await response.json();
-//   return customersInfo;
-// }
+// Define database url for easier modification in the future
+const url = "http://localhost:1337/";
 
-// Promise.all([
-//   getCustomersInfo()
-// ]).then(allInfo => {
-//   ReactDOM.render(<App allInfo = {allInfo} />, document.getElementById("root"));
-// })
+//Fetch data from server
+//1. customers and their pets information.
+const getCustomersAndPetsInfo = async function () {
+   const response = await fetch(`${url}customers-and-pets`);
+   var info = await response.json();
+   return info;
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//2. employees information.
+const getEmployeesInfo = async function () {
+   const response = await fetch(`${url}employees`);
+   var info = await response.json();
+   return info;
+};
 
+//3. items for sale information.
+const getItemsForSaleInfo = async function () {
+   const response = await fetch(`${url}items-for-sales`);
+   var info = await response.json();
+   return info;
+};
+
+//4. pets for sale information.
+const getPetsForSaleInfo = async function () {
+   const response = await fetch(`${url}pets-for-sales`);
+   var info = await response.json();
+   return info;
+};
+
+//5. daily appointments information.
+const getAppointmentsInfo = async function () {
+    const response = await fetch(`${url}daily-appointments`);
+    var info = await response.json();
+    return info;
+ };
+
+//6. store income statistics information.
+const getIncomeStatisticsInfo = async function () {
+    const response = await fetch(`${url}income-statistics`);
+    var info = await response.json();
+    return info;
+ };
+
+// Render <App> if all data is successfully retrived from local Strapi
+Promise.all([
+   getCustomersAndPetsInfo(),
+   getEmployeesInfo(),
+   getItemsForSaleInfo(),
+   getPetsForSaleInfo(),
+   getAppointmentsInfo(),
+   getIncomeStatisticsInfo()
+]).then((allInfo) => {
+   console.log(allInfo);
+   ReactDOM.render(<App allInfo={allInfo} />, document.getElementById("root"));
+});
 
 serviceWorker.unregister();
