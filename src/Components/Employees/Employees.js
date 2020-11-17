e884801818c25b46c8e66200107c14be527ceeff
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-11-16 20:03:36 -0700

    completed basic router structure for Employees component

--- src/Components/Employees/Employees.js
+++ src/Components/Employees/Employees.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react'
+import React from "react";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import CreateEmployeeAccount from "./CreateEmployeeAccount/CreateEmployeeAccount";
+import SearchAndEditEmployee from "./SearchAndEditEmployee/SearchAndEditEmployee";
+import "../CommonElements.css";
 
-export default class Employees extends Component {
-    render() {
-        return (
-            <div>
-                <button>Add New Employee</button>
-                <button>Edit Employee</button>
-            </div>
-        )
-    }
+function Employees(props) {
+   return (
+      <Router>
+         <div className="customers">
+            <Switch>
+               <Route path="/create">
+                  <CreateEmployeeAccount url={props.url} />
+               </Route>
+               <Route path="/search">
+                  <SearchAndEditEmployee allInfo={props.allInfo} url={props.url} />
+               </Route>
+            </Switch>
+            <nav className="nav-components">
+               <Link to="/create">
+                  <button>Create New Employee Account</button>
+               </Link>
+               <Link to="/search">
+                  <button>Search/Edit Employee Account</button>
+               </Link>
+            </nav>
+         </div>
+      </Router>
+   );
 }
+
+export default Employees;
