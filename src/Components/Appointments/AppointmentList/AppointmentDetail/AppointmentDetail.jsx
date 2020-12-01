682168c19effb9f293c2390ac3cf7351e0893e5a
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-11-30 21:19:01 -0700

    completed component, similar to CustomerInfo

--- src/Components/Appointments/AppointmentList/AppointmentDetail/AppointmentDetail.jsx
+++ src/Components/Appointments/AppointmentList/AppointmentDetail/AppointmentDetail.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 
 export default function AppointmentDetail(props) {
-    console.log(props.data);
+    // Define a function to trim off the tailing ".000" in time format
+    // reason for this is the time format on strapi is "xx:xx:xx.xxxx", only the starting "xx:xx" is needed
+    const TrimZeros = (time) => {
+        // console.log("time: " + time)
+        time = time.slice(0, 5);
+        // console.log("new time: " + time);
+        return time;
+    };
+
     return (
         <tr className="appointmentDetail">
-            <td>test</td>
+            <td>{`id: ${props.index}`}</td>
+            <td>{props.content.appointment_date}</td>
+            <td>
+                {TrimZeros(props.content.appointment_time_start) +
+                    " - " +
+                    TrimZeros(props.content.appointment_time_end)}
+            </td>
+            <td>
+                {props.content.appointment_customer_first_name +
+                    " " +
+                    props.content.appointment_customer_last_name}
+            </td>
+            <td>
+                <button
+                    onClick={() => {
+                        props.setVisibility("visible");
+                        props.setAccountId(props.content.id);
+                    }}
+                >
+                    Edit
+                </button>
+            </td>
         </tr>
     );
 }
