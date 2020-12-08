ad3915766b653a0fc14df9bb187f69ad7830cf79
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-12-08 10:40:53 -0700

    added a function to show appointments only on the selected date

--- src/Components/Appointments/AppointmentList/AppointmentList.js
+++ src/Components/Appointments/AppointmentList/AppointmentList.js
@@ -1,26 +1,100 @@
 import React, { useState, useEffect } from "react";
 import AppointmentDetail from "./AppointmentDetail/AppointmentDetail";
+import "./AppointmentList.css";
 
 export default function AppointmentList(props) {
+    const date = props.selectedDate.format("YYYY-MM-DD");
+
     // Define states
     const [appointments, setAppointments] = useState(null);
+    const [appointmentId, setAppointmentId] = useState(1);
+    const [visibility, setVisibility] = useState("hidden");
+    const [hasAppointment, setHasAppointment] = useState(false);
 
-    // When component mount, fetch latest data through API, and assign to "customers_and_pets"
+    // When component mount, fetch latest data through API, and assign to "appointments"
+    // In BookAppointment.jsx, show only those appointments on selectedDate, ie. response.filter...
+    // Otherwise, show all appointments
     useEffect(() => {
         fetch(`${props.url}daily-appointments`)
             .then((response) => response.json())
             .then((response) => {
-                setAppointments(response[0]);
+                // filter the appointments on the selected date
+                var appointmentsOnSelectedDate = response.filter((current) => {
+                    return current.appointment_date === date;
+                });
+                console.log("appointmentsOnSelectedDate");
+                console.log(appointmentsOnSelectedDate);
+
+                // if date is provided, and response's length is not 0, then set hasAppointment to true
+                if (date && appointmentsOnSelectedDate.length !== 0) {
+                    setHasAppointment(true);
+                    setAppointments(
+                        response.filter((current) => {
+                            return current.appointment_date === date;
+                        })
+                    );
+                    console.log("appointments");
+                    console.log(appointments);
+                } else if (!date && appointmentsOnSelectedDate.length !== 0) {
+                    setHasAppointment(true);
+                    setAppointments(response);
+                } else if (date && appointmentsOnSelectedDate.length === 0) {
+                    setHasAppointment(false);
+                }
             })
             .catch((error) => {
-                console.log("error");
+                console.log("error:");
                 console.log(error);
             });
-    }, []);
+    }, [date]);
 
-    return (
-        <div>
-            <AppointmentDetail data={appointments} />
-        </div>
-    );
+    // Render appointment list or "No Appointments" based on hasAppointment
+    if (hasAppointment) {
+        return (
+            <section className="appointmentList">
+                <table>
+                    <thead>
+                        <tr className="appointmentList_tableHead">
+                            <th>ID</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Customer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {appointments &&
+                            appointments.map((content, index) => {
+                                return (
+                                    <AppointmentDetail
+                                        content={content}
+                                        index={index}
+                                        key={index}
+                                        setVisibility={setVisibility}
+                                        setAppointmentId={setAppointmentId}
+                                    />
+                                );
+                            })}
+                    </tbody>
+                </table>
+            </section>
+        );
+    } else {
+        return (
+            <section className="appointmentList">
+                <table>
+                    <thead>
+                        <tr className="appointmentList_tableHead">
+                            <th>ID</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Customer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <td colspan="4">No Appointments</td>
+                    </tbody>
+                </table>
+            </section>
+        );
+    }
 }
