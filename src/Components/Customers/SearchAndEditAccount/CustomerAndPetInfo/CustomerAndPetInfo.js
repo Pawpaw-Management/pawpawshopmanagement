0f72b8911e5d16b0d5d39a63de63a0c5ded5aca2
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-12-14 10:37:29 -0700

    added conditional rendering, added Choose button to change customer account id

--- src/Components/Customers/SearchAndEditAccount/CustomerAndPetInfo/CustomerAndPetInfo.js
+++ src/Components/Customers/SearchAndEditAccount/CustomerAndPetInfo/CustomerAndPetInfo.js
@@ -2,34 +2,67 @@ import React from "react";
 import "./CustomerAndPetInfo.css";
 
 export default function CustomerAndPetInfo(props) {
-    return (
-        <tr className="eachCustomerAndPetInfo">
-            <td>{`id: ${props.index + 1}`}</td>
-            <td>
-                {props.content.customer_first_name +
-                    " " +
-                    props.content.customer_last_name}
-            </td>
-            <td>
-                {props.content.customer_phone
-                    ? props.content.customer_phone
-                    : "no info provided"}
-            </td>
-            <td>
-                {props.content.customer_email
-                    ? props.content.customer_email
-                    : "no info provided"}
-            </td>
-            <td>
-                <button
-                    onClick={() => {
-                        props.setVisibility("visible");
-                        props.setAccountId(props.content.id);
-                    }}
-                >
-                    Edit
-                </button>
-            </td>
-        </tr>
-    );
+    if (props.scenario == "AddAppointment") {
+        return (
+            <tr className="eachCustomerAndPetInfo">
+                <td>{`id: ${props.index + 1}`}</td>
+                <td>
+                    {props.content.customer_first_name +
+                        " " +
+                        props.content.customer_last_name}
+                </td>
+                <td>
+                    {props.content.customer_phone
+                        ? props.content.customer_phone
+                        : "no info provided"}
+                </td>
+                <td>
+                    {props.content.customer_email
+                        ? props.content.customer_email
+                        : "no info provided"}
+                </td>
+                <td>
+                    <button
+                        onClick={() => {
+                            props.setVisibilityCustomer("hidden");
+                            props.setAccountId(props.content.id);
+                        }}
+                    >
+                        Choose
+                    </button>
+                </td>
+            </tr>
+        );
+    } else {
+        return (
+            <tr className="eachCustomerAndPetInfo">
+                <td>{`id: ${props.index + 1}`}</td>
+                <td>
+                    {props.content.customer_first_name +
+                        " " +
+                        props.content.customer_last_name}
+                </td>
+                <td>
+                    {props.content.customer_phone
+                        ? props.content.customer_phone
+                        : "no info provided"}
+                </td>
+                <td>
+                    {props.content.customer_email
+                        ? props.content.customer_email
+                        : "no info provided"}
+                </td>
+                <td>
+                    <button
+                        onClick={() => {
+                            props.setVisibility("visible");
+                            props.setAccountId(props.content.id);
+                        }}
+                    >
+                        Edit
+                    </button>
+                </td>
+            </tr>
+        );
+    }
 }
