6ed73862ecc5d6358ec817c0ad8383ff33241915
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-12-14 10:34:07 -0700

    added scenario for conditional rendering

--- src/Components/Customers/Customers.js
+++ src/Components/Customers/Customers.js
@@ -5,28 +5,31 @@ import SearchAccount from "./SearchAndEditAccount/SearchAndEditAccount";
 import "../CommonElements.css";
 
 function Customers(props) {
-   return (
-      <Router>
-         <div className="customers">
-            <Switch>
-               <Route path="/create">
-                  <CreateAccount url={props.url} />
-               </Route>
-               <Route path="/search">
-                  <SearchAccount url={props.url} />
-               </Route>
-            </Switch>
-            <nav className="nav-components">
-               <Link to="/create">
-                  <button>Create New Customer Account</button>
-               </Link>
-               <Link to="/search">
-                  <button>Search/Edit Customer Account</button>
-               </Link>
-            </nav>
-         </div>
-      </Router>
-   );
+    // Define scenario to conditionally render content in <SearchAccount>
+    const scenario = "Customers";
+
+    return (
+        <Router>
+            <div className="customers">
+                <Switch>
+                    <Route path="/create">
+                        <CreateAccount url={props.url} />
+                    </Route>
+                    <Route path="/search">
+                        <SearchAccount url={props.url} scenario={scenario} />
+                    </Route>
+                </Switch>
+                <nav className="nav-components">
+                    <Link to="/create">
+                        <button>Create New Customer Account</button>
+                    </Link>
+                    <Link to="/search">
+                        <button>Search/Edit Customer Account</button>
+                    </Link>
+                </nav>
+            </div>
+        </Router>
+    );
 }
 
 export default Customers;
