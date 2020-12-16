ce2562085b5f1247ab315ffbad26a6f8805985e0
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-12-16 10:41:31 -0700

    changed page layout to introduce AddAppointment component

--- src/Components/Appointments/BookAppointment/BookAppointment.jsx
+++ src/Components/Appointments/BookAppointment/BookAppointment.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "./Calendar/Calendar.js";
-import AppointmentList from "../AppointmentList/AppointmentList"
-import "./BookAppointment.css"
+import AppointmentList from "../AppointmentList/AppointmentList";
+import AddAppointment from "./AddAppointment/AddAppointment";
+import "./BookAppointment.css";
 
 export default function BookAppointment(props) {
+    console.log(props.url)
+
     return (
         <section className="bookAppointment">
-            <Calendar
+            <div class="bookAppointment_calendar_and_form">
+                <Calendar
+                    selectedDate={props.selectedDate}
+                    onChange={props.setSelectedDate}
+                />
+                <AddAppointment
+                    url={props.url}
+                    selectedDate={props.selectedDate}
+                />
+            </div>
+            {/* <AppointmentList
+                url={props.url}
                 selectedDate={props.selectedDate}
-                onChange={props.setSelectedDate}
-            />
-            <AppointmentList url={props.url} selectedDate={props.selectedDate} />
+            /> */}
         </section>
     );
 }
