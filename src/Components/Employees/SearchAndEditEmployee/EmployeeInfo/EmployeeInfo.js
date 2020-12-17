3c51b9497767b41fa9a2f822d875a5905ba7129c
Pawpaw-Management <pawpawmanagement@gmail.com>
2020-12-16 17:14:21 -0700

    now can conditionally render content when used in <AddAppointment>

--- src/Components/Employees/SearchAndEditEmployee/EmployeeInfo/EmployeeInfo.js
+++ src/Components/Employees/SearchAndEditEmployee/EmployeeInfo/EmployeeInfo.js
@@ -1,35 +1,68 @@
-import React from 'react'
-import "./EmployeeInfo.css"
+import React from "react";
+import "./EmployeeInfo.css";
 
 export default function EmployeeInfo(props) {
-    return (
-        <tr className="eachEmployeeInfo">
-            <td>{`id: ${props.index + 1}`}</td>
-            <td>
-                {props.content.employee_first_name +
-                    " " +
-                    props.content.employee_last_name}
-            </td>
-            <td>
-                {props.content.employee_phone
-                    ? props.content.employee_phone
-                    : "no info provided"}
-            </td>
-            <td>
-                {props.content.employee_email
-                    ? props.content.employee_email
-                    : "no info provided"}
-            </td>
-            <td>
-                <button
-                    onClick={() => {
-                        props.setVisibility("visible");
-                        props.setAccountId(props.content.id);
-                    }}
-                >
-                    Edit
-                </button>
-            </td>
-        </tr>
-    )
+    if (props.scenario === "AddAppointment") {
+        return (
+            <tr className="eachEmployeeInfo">
+                <td>{`id: ${props.index + 1}`}</td>
+                <td>
+                    {props.content.employee_first_name +
+                        " " +
+                        props.content.employee_last_name}
+                </td>
+                <td>
+                    {props.content.employee_phone
+                        ? props.content.employee_phone
+                        : "no info provided"}
+                </td>
+                <td>
+                    {props.content.employee_email
+                        ? props.content.employee_email
+                        : "no info provided"}
+                </td>
+                <td>
+                    <button
+                        onClick={() => {
+                            props.setVisibilityProvider(false);
+                            props.setProviderId(props.content.id);
+                        }}
+                    >
+                        Choose
+                    </button>
+                </td>
+            </tr>
+        );
+    } else {
+        return (
+            <tr className="eachEmployeeInfo">
+                <td>{`id: ${props.index + 1}`}</td>
+                <td>
+                    {props.content.employee_first_name +
+                        " " +
+                        props.content.employee_last_name}
+                </td>
+                <td>
+                    {props.content.employee_phone
+                        ? props.content.employee_phone
+                        : "no info provided"}
+                </td>
+                <td>
+                    {props.content.employee_email
+                        ? props.content.employee_email
+                        : "no info provided"}
+                </td>
+                <td>
+                    <button
+                        onClick={() => {
+                            props.setVisibility("visible");
+                            props.setAccountId(props.content.id);
+                        }}
+                    >
+                        Edit
+                    </button>
+                </td>
+            </tr>
+        );
+    }
 }
--- src/Components/Employees/SearchAndEditEmployee/SearchAndEditEmployee.js
+++ src/Components/Employees/SearchAndEditEmployee/SearchAndEditEmployee.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import EmployeeInfo from "./EmployeeInfo/EmployeeInfo";
 import InfoEditor from "./InfoEditor/InfoEditor";
 import "./SearchAndEditEmployee.css";
+import "../../CommonElements.css";
 
 const SearchAndEditEmployee = (props) => {
     // Define useState for <Refresh> to update state here
@@ -18,52 +19,95 @@ const SearchAndEditEmployee = (props) => {
         fetch(`${props.url}employees`)
             .then((response) => response.json())
             .then((response) => {
-                setEmployees({ employees: response });
+                setEmployees(response);
             })
             .catch((error) => {
                 console.log(error);
             });
     }, []);
 
-    // Shorten the long name
-    const info = employees.employees;
-
-    return (
-        <section className="searchAndEditEmployee">
-            <h1>All Employee Accounts</h1>
-            <table className="employeeList">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Employee Name</th>
-                        <th>Phone</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {info &&
-                        info.map((content, index) => {
-                            return (
-                                <EmployeeInfo
-                                    content={content}
-                                    key={index}
-                                    index={index}
-                                    setVisibility={setVisibility}
-                                    setAccountId={setAccountId}
-                                />
-                            );
-                        })}
-                </tbody>
-            </table>
-            <InfoEditor
-                visibility={visibility}
-                accountId={accountId}
-                url={props.url}
-                employees={employees}
-                setVisibility={setVisibility}
-            />
-        </section>
-    );
+    // Render elements according to scenario:
+    // if "AddAppointment", show "choose" button, cannot edit data
+    // if "Customers", show "edit" button, can edit data
+    if (props.scenario === "AddAppointment") {
+        return (
+            <section className="searchAndEditEmployee window">
+                <h1>All Employee Accounts</h1>
+                <button
+                    className="button_esc"
+                    onClick={() => {
+                        props.setVisibilityProvider(false);
+                    }}
+                >
+                    Esc
+                </button>
+                <table className="employeeList">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Employee Name</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {employees &&
+                            employees.map((content, index) => {
+                                return (
+                                    <EmployeeInfo
+                                        scenario={props.scenario}
+                                        content={content}
+                                        key={index}
+                                        index={index}
+                                        setProviderId={props.setProviderId}
+                                        setVisibilityProvider={
+                                            props.setVisibilityProvider
+                                        }
+                                    />
+                                );
+                            })}
+                    </tbody>
+                </table>
+            </section>
+        );
+    } else if (props.scenario === "Employees") {
+        return (
+            <section className="searchAndEditEmployee">
+                <h1>All Employee Accounts</h1>
+                <table className="employeeList">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Employee Name</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {employees &&
+                            employees.map((content, index) => {
+                                return (
+                                    <EmployeeInfo
+                                        content={content}
+                                        key={index}
+                                        index={index}
+                                        setVisibility={setVisibility}
+                                        setAccountId={setAccountId}
+                                    />
+                                );
+                            })}
+                    </tbody>
+                </table>
+                <InfoEditor
+                    visibility={visibility}
+                    accountId={accountId}
+                    url={props.url}
+                    employees={employees}
+                    setVisibility={setVisibility}
+                />
+            </section>
+        );
+    }
 };
 
 export default SearchAndEditEmployee;
