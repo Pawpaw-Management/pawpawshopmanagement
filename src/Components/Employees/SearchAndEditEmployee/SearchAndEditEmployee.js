e615440cac3c99dc185d75d6a73cb3aa65185473
cafeoplay-github <cafeoplay-github@gmail.com>
2021-05-01 21:17:16 -0600

    added 1 more listener to useEffect

--- src/Components/Employees/SearchAndEditEmployee/SearchAndEditEmployee.js
+++ src/Components/Employees/SearchAndEditEmployee/SearchAndEditEmployee.js
@@ -24,7 +24,7 @@ const SearchAndEditEmployee = (props) => {
             .catch((error) => {
                 console.log(error);
             });
-    }, []);
+    }, [visibility]);
 
     // Render elements according to scenario:
     // if "AddAppointment", show "choose" button, cannot edit data
