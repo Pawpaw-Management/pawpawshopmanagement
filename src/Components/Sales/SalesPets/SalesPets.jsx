3a0579ac1557e568f3d9c319c9331d55a6a46510
cafeoplay-github <cafeoplay-github@gmail.com>
2021-05-02 19:05:13 -0600

    changed component structure

--- src/Components/Sales/SalesPets/SalesPets.jsx
+++ src/Components/Sales/SalesPets/SalesPets.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
 import AddPet from "./AddNewPet/AddPet"
 import EditPet from "./ListOfPets/ListOfPets"
-import SellPet from "./SellPet/SellPet"
+import ListOfSoldPet from "./ListOfSoldPet/ListOfSoldPet"
 import "./SalesPets.css";
 
 export default function SalesPets(props) {
@@ -22,8 +22,8 @@ export default function SalesPets(props) {
                             url={props.url}
                         />
                     </Route>
-                    <Route path="/sellpet">
-                        <SellPet
+                    <Route path="/soldpet">
+                        <ListOfSoldPet
                             url={props.url}
                         />
                     </Route>
@@ -35,8 +35,8 @@ export default function SalesPets(props) {
                     <Link to="/editpet">
                         <button>Edit Pet</button>
                     </Link>
-                    <Link to="/sellpet">
-                        <button>Sell Pet</button>
+                    <Link to="/soldpet">
+                        <button>Sold Pet</button>
                     </Link>
                 </nav>
             </section>
