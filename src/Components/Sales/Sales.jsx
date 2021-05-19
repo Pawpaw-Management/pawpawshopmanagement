8d3f585a242c1c24e47f41bf1e8a79576286e4e7
cafeoplay-github <cafeoplay-github@gmail.com>
2021-05-19 16:36:54 -0600

    added payment component

--- src/Components/Sales/Sales.jsx
+++ src/Components/Sales/Sales.jsx
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
 import SalesProducts from "./SalesProducts/SalesProducts";
-import SalesPets from "./SalesPets/SalesPets"
+import SalesPets from "./SalesPets/SalesPets";
+import Payments from "./Payment/Payment";
 
 export default function Sales(props) {
-    // Define states
-
     return (
         <Router>
             <section className="appointments">
                 <Switch>
                     <Route path="/products">
-                        <SalesProducts
-                            url={props.url}
-                        />
+                        <SalesProducts url={props.url} />
                     </Route>
                     <Route path="/pets">
-                        <SalesPets
-                            url={props.url}
-                        />
+                        <SalesPets url={props.url} />
+                    </Route>
+                    <Route path="/payments">
+                        <Payments url={props.url} />
                     </Route>
                 </Switch>
                 <nav className="nav-components">
@@ -27,6 +25,9 @@ export default function Sales(props) {
                     </Link>
                     <Link to="/pets">
                         <button>Pets</button>
+                    </Link>
+                    <Link to="/payments">
+                        <button>Invoice Generator</button>
                     </Link>
                 </nav>
             </section>
