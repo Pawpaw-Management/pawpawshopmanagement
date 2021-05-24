e173b8ae0c6f1f0834fbecb8a59e8566ecf1f4c6
cafeoplay-github <cafeoplay-github@gmail.com>
2021-05-24 16:38:31 -0600

    account search feature wip

--- src/Components/Customers/SearchAndEditAccount/SearchAndEditAccount.js
+++ src/Components/Customers/SearchAndEditAccount/SearchAndEditAccount.js
@@ -9,11 +9,47 @@ const SearchAccount = (props) => {
     const [customers_and_pets, setCustomersAndPets] = useState([]);
 
     // Define state for <InfoEditor> to popup and disappear
-    const [visibility, setVisibility] = useState("hidden");
+    const [visibility, setVisibility] = useState(false);
 
     // Define state for <InfoEditor> to edit the account clicked by user
     const [accountId, setAccountId] = useState(33);
-    console.log(`accountId: ${accountId}`);
+
+    // // W.I.P.
+    // // Define state and onChange handler for search bar
+    // const [namePhoneEmail, setNamePhoneEmail] = useState("");
+    // const changeNamePhoneEmail = (event) =>
+    //     setNamePhoneEmail(event.target.value);
+    // var searchResult;
+    // if (namePhoneEmail !== "" && customers_and_pets !== []) {
+    //     console.log(customers_and_pets[0].customer_first_name);
+    //     console.log(customers_and_pets[0].customer_first_name.toLowerCase().includes("a"));
+    //     searchResult = customers_and_pets.filter((customer) => {
+    //         // console.log(customer.customer_first_name.toLowerCase().includes("a"))
+    //         // console.log("namePhoneEmail: " + namePhoneEmail)
+    //         customer.customer_first_name
+    //             .toLowerCase()
+    //             .includes("a".toLowerCase());
+    //     });
+    // }
+    // var customers_and_pets_filtered =
+    //     namePhoneEmail === "" ? customers_and_pets : searchResult;
+    // console.log(namePhoneEmail !== "" && customers_and_pets !== []);
+    // console.log("searchResult");
+    // console.log(searchResult);
+    // console.log("==========");
+    // // W.I.P.
+
+    // Define a function to fetch latest data through API, and assign to "customers_and_pets"
+    const fetchData = () => {
+        fetch(`${props.url}customers-and-pets`)
+            .then((response) => response.json())
+            .then((response) => {
+                setCustomersAndPets(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     // When component mount, fetch latest data through API, and assign to "customers_and_pets"
     useEffect(() => {
@@ -33,6 +69,14 @@ const SearchAccount = (props) => {
     if (props.scenario === "AddAppointment") {
         return (
             <section className="searchAndEditCustomer appointmentWindow">
+                {/* <div className="search-customer-account">
+                    <label htmlFor="customer__name-phone-email">Search</label>
+                    <input
+                        name="customer__name-phone-email"
+                        value={namePhoneEmail}
+                        onChange={changeNamePhoneEmail}
+                    />
+                </div> */}
                 <h1>Customer List</h1>
                 <button
                     className="button_esc"
@@ -75,7 +119,34 @@ const SearchAccount = (props) => {
     } else if (props.scenario === "Customers") {
         return (
             <section className="searchAndEditCustomer">
-                <h1>All Customer Accounts</h1>
+                <div className="title-and-refresh-button">
+                    <h1>All Customer Accounts</h1>
+                    <button
+                        className="refresh-button"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            fetchData();
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            height="24"
+                        >
+                            <path fill="none" d="M0 0h24v24H0z" />
+                            <path d="M5.463 4.433A9.961 9.961 0 0 1 12 2c5.523 0 10 4.477 10 10 0 2.136-.67 4.116-1.81 5.74L17 12h3A8 8 0 0 0 6.46 6.228l-.997-1.795zm13.074 15.134A9.961 9.961 0 0 1 12 22C6.477 22 2 17.523 2 12c0-2.136.67-4.116 1.81-5.74L7 12H4a8 8 0 0 0 13.54 5.772l.997 1.795z" />
+                        </svg>
+                    </button>
+                </div>
+                {/* <div className="search-customer-account">
+                    <label htmlFor="customer__name-phone-email">Search</label>
+                    <input
+                        name="customer__name-phone-email"
+                        value={namePhoneEmail}
+                        onChange={ChangeNamePhoneEmail}
+                    />
+                </div> */}
                 <table className="customerList">
                     <thead>
                         <tr>
@@ -109,13 +180,14 @@ const SearchAccount = (props) => {
                             })}
                     </tbody>
                 </table>
-                <InfoEditor
-                    visibility={visibility}
-                    accountId={accountId}
-                    url={props.url}
-                    customers_and_pets={customers_and_pets}
-                    setVisibility={setVisibility}
-                />
+                {visibility ? (
+                    <InfoEditor
+                        accountId={accountId}
+                        url={props.url}
+                        customers_and_pets={customers_and_pets}
+                        setVisibility={setVisibility}
+                    />
+                ) : null}
             </section>
         );
     }
