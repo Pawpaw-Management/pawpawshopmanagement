b6ba6aab7e4cef04b58b73a54242b302473bcae9
cafeoplay-github <cafeoplay-github@gmail.com>
2021-05-26 21:57:31 -0600

    added pet photo

--- src/Components/Customers/SearchAndEditAccount/CustomerAndPetInfo/CustomerAndPetInfo.js
+++ src/Components/Customers/SearchAndEditAccount/CustomerAndPetInfo/CustomerAndPetInfo.js
@@ -3,17 +3,23 @@ import "./CustomerAndPetInfo.css";
 
 export default function CustomerAndPetInfo(props) {
     // Props.url has a "/" at the end, need to remove it before using it in next step
-    const processed_url = props.url.slice(0, -1)
-    console.log(processed_url)
+    const processed_url = props.url.slice(0, -1);
+    console.log(processed_url);
 
     // If an account has pet_photo, then display it; if not, display "no photo provided"
-    let pet_photo
-    if (props.content.pet_photo !==null) {
-        pet_photo = <img className={"eachCustomerAndPetInfo-photo"} src={processed_url + props.content.pet_photo.url} alt="customer's pet" />
+    let pet_photo;
+    if (props.content.pet_photo !== null) {
+        pet_photo = (
+            <img
+                className={"eachCustomerAndPetInfo-photo"}
+                src={processed_url + props.content.pet_photo.url}
+                alt="customer's pet"
+            />
+        );
     } else {
-        pet_photo = <span>no photo provided</span>
+        pet_photo = <span>no photo provided</span>;
     }
-    
+
     // Conditional rendering based on props.scenario
     if (props.scenario === "AddAppointment") {
         return (
@@ -55,9 +61,7 @@ export default function CustomerAndPetInfo(props) {
                         " " +
                         props.content.customer_last_name}
                 </td>
-                <td>
-                    {pet_photo}
-                </td>
+                <td>{pet_photo}</td>
                 <td>
                     {props.content.customer_phone
                         ? props.content.customer_phone
@@ -71,7 +75,7 @@ export default function CustomerAndPetInfo(props) {
                 <td>
                     <button
                         onClick={() => {
-                            props.setVisibility("visible");
+                            props.setVisibility(true);
                             props.setAccountId(props.content.id);
                         }}
                     >
