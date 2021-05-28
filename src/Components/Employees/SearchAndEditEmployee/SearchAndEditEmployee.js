889162e742365aef6ced2b00f8d294b0b59877d4
cafeoplay-github <cafeoplay-github@gmail.com>
2021-05-27 20:25:26 -0600

    added refresh button, changed render method rom style-hidden to conditional rendering

--- src/Components/Employees/SearchAndEditEmployee/SearchAndEditEmployee.js
+++ src/Components/Employees/SearchAndEditEmployee/SearchAndEditEmployee.js
@@ -9,10 +9,13 @@ const SearchAndEditEmployee = (props) => {
     const [employees, setEmployees] = useState([]);
 
     // Define useState for <InfoEditor> to popup and disappear
-    const [visibility, setVisibility] = useState("hidden");
+    const [visibility, setVisibility] = useState(false);
 
     // Define useState for <InfoEditor> to edit the account clicked by user
-    const [accountId, setAccountId] = useState(9);
+    const [accountId, setAccountId] = useState(0);
+    console.log("accountId: " + accountId)
+
+    const [refresh, setRefresh] = useState(true);
 
     // When component mount, fetch latest data through API, and assign to "customers_and_pets"
     useEffect(() => {
@@ -24,7 +27,7 @@ const SearchAndEditEmployee = (props) => {
             .catch((error) => {
                 console.log(error);
             });
-    }, [visibility]);
+    }, [visibility, refresh]);
 
     // Render elements according to scenario:
     // if "AddAppointment", show "choose" button, cannot edit data
@@ -75,7 +78,26 @@ const SearchAndEditEmployee = (props) => {
     } else if (props.scenario === "Employees") {
         return (
             <section className="searchAndEditEmployee">
-                <h1>All Employee Accounts</h1>
+                <div className="title-and-refresh-button">
+                    <h1>All Employee Accounts</h1>
+                    <button
+                        className="refresh-button"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setRefresh(!refresh);
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            height="24"
+                        >
+                            <path fill="none" d="M0 0h24v24H0z" />
+                            <path d="M5.463 4.433A9.961 9.961 0 0 1 12 2c5.523 0 10 4.477 10 10 0 2.136-.67 4.116-1.81 5.74L17 12h3A8 8 0 0 0 6.46 6.228l-.997-1.795zm13.074 15.134A9.961 9.961 0 0 1 12 22C6.477 22 2 17.523 2 12c0-2.136.67-4.116 1.81-5.74L7 12H4a8 8 0 0 0 13.54 5.772l.997 1.795z" />
+                        </svg>
+                    </button>
+                </div>
                 <table className="employeeList">
                     <thead>
                         <tr>
@@ -100,13 +122,15 @@ const SearchAndEditEmployee = (props) => {
                             })}
                     </tbody>
                 </table>
-                <InfoEditor
-                    visibility={visibility}
-                    accountId={accountId}
-                    url={props.url}
-                    employees={employees}
-                    setVisibility={setVisibility}
-                />
+                {visibility ? (
+                    <InfoEditor
+                        visibility={visibility}
+                        accountId={accountId}
+                        url={props.url}
+                        employees={employees}
+                        setVisibility={setVisibility}
+                    />
+                ) : null}
             </section>
         );
     }
