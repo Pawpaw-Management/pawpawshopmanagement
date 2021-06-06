af45e2a519ad07a202832b65bb0d1017af6fba16
cafeoplay-github <cafeoplay-github@gmail.com>
2021-06-06 15:39:00 -0600

    reworked this component as a navigating component

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,43 +1,32 @@
-import React, { useState, useEffect } from "react";
-import BarChart from "./BarChart/BarChart";
-import Incomes from "./Incomes/Incomes";
-import accounting from "./Accounting/Accounting";
-import "./Statistics.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import DailyStatistics from "./DailyStatistics/DailyStatistics";
+import MonthlyStatistics from "./MonthlyStatistics/MonthlyStatistics";
+import "../CommonElements.css"
 
 export default function Statistics(props) {
-    // Define states
-    const [incomes, setIncomes] = useState([]);
-
-    // When this component mounts, fetch data
-    useEffect(() => {
-        fetch(`${props.url}income-statistics`)
-            .then((response) => response.json())
-            .then((response) => {
-                setIncomes(response); //assign the fetched data to incomes
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
-
-    // take income_total and creation date from json and assign to 2 variables
-    var incomes_value = incomes.map((item) => item.Income_Total);
-    var incomes_date = incomes.map((item) => item.created_at.slice(0, 10)); //Only the date is needed here, so slice off the time
-    // console.log(incomes_value)
-    // console.log(incomes_date)
     return (
-        <div id="accounting-report">
-            <div id="accounting-report-button">
-                <span>Please click this button on the <b>FIRST DAY</b> every month</span>
-                <button onClick={() => accounting()}>
-                    Generate Monthly Report
-                </button>
-            </div>
-            <BarChart
-                incomes_value={incomes_value}
-                incomes_date={incomes_date}
-            />
-            <Incomes incomes={incomes} url={props.url} />
-        </div>
+        <Router>
+            <section className="statistics">
+                <Switch>
+                    <Route path="/dailystatistics">
+                        <DailyStatistics url={props.url} />
+                    </Route>
+                </Switch>
+                <Switch>
+                    <Route path="/monthlystatistics">
+                        <MonthlyStatistics url={props.url} />
+                    </Route>
+                </Switch>
+                <nav className="nav-components">
+                    <Link to="/dailystatistics">
+                        <button>Daily Statistics</button>
+                    </Link>
+                    <Link to="/monthlystatistics">
+                        <button>Monthly Statistics</button>
+                    </Link>
+                </nav>
+            </section>
+        </Router>
     );
 }
