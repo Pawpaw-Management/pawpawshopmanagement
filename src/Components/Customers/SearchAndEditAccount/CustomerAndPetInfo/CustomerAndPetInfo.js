a70799df5f4a462dddf4b3623fdee7cb82f10dc8
cafeoplay-github <cafeoplay-github@gmail.com>
2021-07-24 00:04:02 -0600

    added a function to format phone numbers

--- src/Components/Customers/SearchAndEditAccount/CustomerAndPetInfo/CustomerAndPetInfo.js
+++ src/Components/Customers/SearchAndEditAccount/CustomerAndPetInfo/CustomerAndPetInfo.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./CustomerAndPetInfo.css";
 
 export default function CustomerAndPetInfo(props) {
     // Props.url has a "/" at the end, need to remove it before using it in next step
     const processed_url = props.url.slice(0, -1);
-    console.log(processed_url);
 
     // If an account has pet_photo, then display it; if not, display "no photo provided"
     let pet_photo;
@@ -20,15 +19,41 @@ export default function CustomerAndPetInfo(props) {
         pet_photo = <span>no photo provided</span>;
     }
 
+    // Define a function to format phone numbers into (xxx) xxx - xxxx
+    const formatToPhone = (phoneNumber) => {
+        const input = phoneNumber.replace(/\D/g, "").substring(0, 10); // First ten digits of input only
+        const areaCode = input.substring(0, 3);
+        const middle = input.substring(3, 6);
+        const last = input.substring(6, 10);
+
+        if (input.length > 6) {
+            phoneNumber = `(${areaCode}) ${middle} - ${last}`;
+        } else if (input.length > 3) {
+            phoneNumber = `(${areaCode}) ${middle}`;
+        } else if (input.length > 0) {
+            phoneNumber = `(${areaCode}`;
+        }
+
+        return phoneNumber
+    };
+
+    // Define state
+    const [formattedPhoneNumber, setFormattedPhoneNumber] = useState("");
+
+    // Update formattedPhoneNumber when props.content.customer_phone changes
+    useEffect(() => {
+        if (props.content.customer_phone) {
+            setFormattedPhoneNumber(formatToPhone(props.content.customer_phone));
+        }
+    }, [props.content.customer_phone]);
+
     // Conditional rendering based on props.scenario
     if (props.scenario === "AddAppointment") {
         return (
             <tr className="eachCustomerAndPetInfo">
                 <td>{`id: ${props.index + 1}`}</td>
                 <td>
-                    {props.content.customer_first_name +
-                        " " +
-                        props.content.customer_last_name}
+                    {props.content.customer_first_name + " " + props.content.customer_last_name}
                 </td>
                 <td>
                     {props.content.customer_phone
@@ -57,16 +82,10 @@ export default function CustomerAndPetInfo(props) {
             <tr className="eachCustomerAndPetInfo">
                 <td>{`id: ${props.index + 1}`}</td>
                 <td>
-                    {props.content.customer_first_name +
-                        " " +
-                        props.content.customer_last_name}
+                    {props.content.customer_first_name + " " + props.content.customer_last_name}
                 </td>
                 <td>{pet_photo}</td>
-                <td>
-                    {props.content.customer_phone
-                        ? props.content.customer_phone
-                        : "no info provided"}
-                </td>
+                <td>{props.content.customer_phone ? formattedPhoneNumber : "no info provided"}</td>
                 <td>
                     {props.content.customer_email
                         ? props.content.customer_email
