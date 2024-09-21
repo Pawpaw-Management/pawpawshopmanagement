68ae03f6c8ca4a2e2e207697fbc6bc4084172a90
Pawpaw-Management <60239878+IvanTheWonderful@users.noreply.github.com>
2024-09-21 10:35:19 -0600

    remove unused code

--- src/Components/ReuseableComponents/PaginationControl.js
+++ src/Components/ReuseableComponents/PaginationControl.js
@@ -1,14 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import React, { useState } from "react";
+import React from "react";
 import "./PaginationControl.css";
 
 const PaginationControl = (props) => {
     const { currentPage, setCurrentPage, totalPageNumber } = props;
-    console.log("currentPage inside pagination: ", currentPage);
-
-    // Define state to disable "Go!" button
-    const [pageNumberIsInvalid, setPageNumberIsInvalid] = useState(false);
 
     // Define onClick for ">" and "<" button
     const onClickNext = () => {
@@ -18,7 +14,6 @@ const PaginationControl = (props) => {
         setCurrentPage(currentPage - 1);
     };
 
-    const shouldDisableGoButton = pageNumberIsInvalid || Number(currentPage) <= 0;
     return (
         <div className="pagination-control">
             <button disabled={Number(currentPage) <= 1} onClick={onClickPrev}>
